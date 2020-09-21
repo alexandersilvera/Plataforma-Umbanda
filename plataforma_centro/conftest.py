bc15af7567199cefcd1a4f292a64a26afdd7f5c6
alexander <alexandersivera@hotmail.com>
2020-09-21 20:08:51 -0300

    Primer commit de inico

---
+++ plataforma_centro/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from plataforma_centro.users.models import User
+from plataforma_centro.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
